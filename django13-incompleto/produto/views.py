c0ea2950f633b4bda9de4e7139a29fe3e2be1082
bdsp59 <brunodsp32@gmail.com>
2022-06-20 19:06:13 -0300

    Estudos em Django

---
+++ django13-incompleto/produto/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+def index(request):
+    frase = "Esta frase está sendo exibida pela página index.html de produto."
+    return render(request, 'produto/index.html', {'frase': frase})
